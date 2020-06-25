4a6eec82686730cd648c6536e401e8a13b0b0c90
Daria Chekhovskaya <66278498+xDoshis@users.noreply.github.com>
2020-06-25 14:30:22 -0400

    Add files via upload

---
+++ examples/run.js
@@ -0,0 +1,42 @@
+var  mqtt2opc = require("../mqtt2opcua").run;
+var  Events = require('events').EventEmitter;
+
+forward = new Events();
+backward = new Events();
+
+// Set up forward and reverse data conversion functions
+// These are based on topic path - the finer grained pattern will be used.
+// Examples below
+
+forward.on("$SYS/broker/bytes/#", function(payload) {
+    return {
+            dataType: "Int32",
+            value: parseInt(payload)
+         };
+});
+
+backward.on("$SYS/broker/bytes/#", function(variant) {
+            return {
+                topic:variant.topic,
+                payload:variant.value
+            };
+});
+
+options = {
+    //opcName:"MQTT_Local",
+    //opcHost:"localhost",
+    //opcPort: "4334",
+    mqttHost:"localhost",
+    mqttPort:"8324",
+    mqttUsername:"opcua",
+    mqttPassword:"secretpassword",
+    debug:true,
+    roundtrip:false,	// set to true to limit updates to onMessage (i.e. validate an accuator is set)
+    forward:forward,	// data converter - mqtt -> opcua
+    backward:backward,	// data converter - opcua -> mqtt
+    //topics:['#','$SYS/broker/#'] // Customize to override. These are the default so uncessary.
+};
+
+var server = new mqtt2opc(options);
+
+
---
+++ mqtt2opcua.js
@@ -0,0 +1,241 @@
+"use strict";
+
+var opcua = require("node-opcua"),
+    mqtt = require('mqtt'),
+    Events = require('events').EventEmitter,
+    Qlobber = require('qlobber').Qlobber;
+
+
+var  _debug  = false;
+
+
+
+function debug(s) {
+    if (_debug) {
+        console.log(s);
+    }
+}
+
+
+function Matcher(handlers) {
+    Events.call(this);
+    this.matcher = new Qlobber({
+        separator: '/',     //the character to use in separating words in topics, MQTT uses '/'
+        wildcard_one: '+',  //character to use for matching exactly one non-empty word in a topic, MQTT uses '+'
+        wildcard_some: '#'  //character to use for matching zero or more words in a topci 
+    });
+
+    if (handlers.hasOwnProperty('_events')) {
+        this._events = handlers._events;
+    }
+}
+
+Matcher.prototype = Object.create(Events.prototype);
+Matcher.prototype.match = function (topic) {
+    if (Object.prototype.hasOwnProperty.call(this._events, topic)){
+        return this._events[topic];
+    }
+    var matches = this.matcher.match(topic);
+    return matches.length ? matches.pop() : null;
+};
+
+Matcher.prototype.init = function () {
+    for (var e in this._events) {
+        if (Object.prototype.hasOwnProperty.call(this._events, e)) {
+            this.matcher.add(e, this._events[e]);
+        }
+    }
+};
+
+Matcher.prototype.hasDefault = function () {
+    return this._events['#'] !== undefined;
+};
+
+var run = function (options) {
+
+   _debug = options.debug || false;
+
+ // Forward and backward handlers - see below for example
+    var fhandlers = new Matcher(options.forward),
+        bhandlers = new Matcher(options.backward);
+
+    if (!fhandlers.hasDefault()) {
+        fhandlers.on("/#", function (payload) { // Default backward handler (MQTT -> OPCUA)
+            return {
+                dataType: "String",
+                value: String(payload),
+            };
+        });
+    }
+
+    // Default backward handler
+    if (!bhandlers.hasDefault()) {
+        
+        bhandlers.on("#", function (variant) { // Default backward handler (OPCUA -> MQTT)
+            return {
+                topic:variant.topic,
+                payload:variant.value
+            };
+        });
+    }
+
+    fhandlers.init();
+    bhandlers.init();
+
+
+    // Let's create an instance of OPCUAServer
+
+    var server = new opcua.OPCUAServer({
+        hostname: options.opcHost || "127.0.0.1",
+        port: options.opcPort || 4334, // the port of the listening socket of the server
+        resourcePath: options.opcName || "/UA/MQTT Bridge Server", // this path will be added to the endpoint resource name
+        buildInfo : {
+            productName: "MQTT2OPCUA Bridge",
+            buildNumber: "0.1",
+            buildDate: new Date(2020, 20, 6)
+        }
+    });
+
+
+    function post_initialize() {
+
+        debug("OPC Server Initialized");
+        var persist = {},    // New persistent store
+            nodes = {},
+            paths = {},
+            flagged = {};
+
+        function onMessage(topic, payload) {
+
+            debug("MQTT onMessage: " + topic + " (" + payload.length + " bytes)");
+
+            var node,
+                handler = fhandlers.match(topic);
+
+            if (!handler) {
+                console.error("No handler defined for " + topic);
+                return;
+            }
+
+            if (nodes.hasOwnProperty(topic)) {
+                persist[topic] = handler(payload).value; // Note: need to handle null responses
+                flagged[topic] = false;
+                return;
+            }
+
+            var sub,
+                path = topic.split("/"),
+                top  = (path[0] || path[1]); // Cater for paths starting with "/"
+
+                const addressSpace = server.engine.addressSpace;
+                const nameSpace = addressSpace.getOwnNamespace();
+
+                if(paths.hasOwnProperty(top) === true){
+                    node = paths[top];
+                }
+                else{
+                    node = nameSpace.addFolder("ObjectsFolder", {browseName: top});
+                }
+
+
+            for (sub in path) {
+                if (sub == 0 |!path.hasOwnProperty(sub)) {
+                    continue;
+                }
+
+                top += "/" + path[sub];
+                if (topic !== top) {
+                    if(paths.hasOwnProperty(top) === true){
+                        node = paths[top];
+                    }
+                    else{
+                        node = nameSpace.addFolder(node, {browseName: path[sub]});
+                    }
+                   
+                } 
+                else {
+                    debug("Creating folders for new topic: " + topic);
+                    persist[topic] = handler(payload).value;
+                    flagged[topic] = false;
+                    try {
+                        nodes[topic] = server.engine.nameSpace.addVariable({
+                            componentOf: node,
+                            browseName: path[sub],
+                            path: topic,
+                            nodeId: "s=" + topic,
+                            dataType: handler(payload).dataType,
+                            value: {
+                                get: function () {
+                                    debug("OPCUA Get: " + topic);
+                                    return (!flagged[topic]) ?
+                                        new opcua.Variant(handler(persist[topic])) :
+                                            opcua.StatusCodes.BadCommunicationError;
+                                    },
+                                set: function (variant) {
+                                    debug("OPCUA Set: " + topic);
+                                    try {
+                                        variant.topic=topic;
+                                        var bhandler = bhandlers.match(topic),
+                                            message  = bhandler(variant);
+                                        if (message.hasOwnProperty("topic") &&
+                                            message.hasOwnProperty("payload")) {
+                                                if (!options.roundtrip)
+                                                    persist[topic] = message.payload;
+                                                else
+                                                    flagged[topic] = true;
+                                                debug("MQTT Publish: " + message.topic.toString() + " (" + message.payload.toString().substr(0,8) + ")" )
+                                                server.mqtt.publish(message.topic.toString(),
+                                                                    message.payload.toString());
+                                            }
+                                    } catch(e){
+                                        console.error(e);
+                                    }
+                                    return opcua.StatusCodes.Good;
+                                }
+                            }
+                        });
+                    } catch (e) {
+                        console.error(e);
+                    }
+                }
+            }
+            return;
+        }
+
+
+        server.start(function (err) {
+	    if (err){
+	       console.error(err);
+	       process.exit(1);
+	    }
+
+	    var endpointUrl = server.endpoints[0].endpointDescriptions()[0].endpointUrl;
+            console.log("OPC Server is now available at: " + endpointUrl +  " ( press CTRL+C to stop)");
+
+            var mqttURL = 'mqtt://' + (options.mqttHost || "localhost") + ":" + (options.mqttPort || "1883");
+
+            if (!(server.mqtt = mqtt.connect(mqttURL,{ username: options.mqttUsername, password: options.mqttPassword}))) {
+                console.error("MQTT Unable to connect");
+                process.exit(1);
+            }
+
+            server.mqtt.on('connect', function () {
+                console.log("MQTT Connected to: %s\n", mqttURL);
+                server.mqtt.subscribe(options.topics || ['$SYS/#', '#']);
+            });
+
+            server.mqtt.on('message', function (topic, message) {
+                onMessage(topic, message);
+            });
+
+            server.mqtt.on('disconnect', function () {
+                debug("MQTT Disconnected ");
+                process.exit(1);
+            });
+        });
+    }
+
+    server.initialize(post_initialize);
+};
+
+module.exports.run = run;
